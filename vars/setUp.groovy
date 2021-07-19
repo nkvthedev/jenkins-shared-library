5c750dea384c7a149d955bc4140a18cebd094e21
NKV <35300907+nkvthedev@users.noreply.github.com>
2021-07-18 22:54:00 -0400

    Update setUp.groovy

--- vars/setUp.groovy
+++ vars/setUp.groovy
@@ -1,3 +1,9 @@
-def call() {
-  echo "Running setup"
+import com.local.Instance
+
+def call(String name) {
+  def i = new Instance(this, name)
+  println i.getAddress()
+  def j = new Instance(this, name)
+  j.setAddress()
+  println i.getAddress()
 }
