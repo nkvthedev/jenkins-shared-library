36a57f9746f46b663e440441ccc2c2e4018b8e0a
NKV <35300907+nkvthedev@users.noreply.github.com>
2021-07-18 23:02:06 -0400

    Update setUp.groovy

--- vars/setUp.groovy
+++ vars/setUp.groovy
@@ -3,6 +3,8 @@ import com.local.Instance
 def call(String name) {  
   def i = new Instance(this, name)
   println i.getAddress()
+  println i.name
+  println i.script
 
   def j = new Instance(this, name)
   j.setAddress()
