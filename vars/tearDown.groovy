db1cf823c53f5e029793c2ef5080420fca118fec
NKV <35300907+nkvthedev@users.noreply.github.com>
2022-06-22 17:22:19 -0400

    Update tearDown.groovy

--- vars/tearDown.groovy
+++ vars/tearDown.groovy
@@ -1,14 +1,21 @@
 def call(String name, Closure block) {
   testname = 'test1'
   this.jobname = 'JOB1'
+  println this.instance
+
   if (name == '') {
     block()
   }
 }
 
+def defineVariables() {
+  def instance = 'instance1'
+}
+
 def disconnect(String name, Closure block) {
   println this.jobname
   println testname
+  println this.instance
   if (name == '') {
     block()
   }
