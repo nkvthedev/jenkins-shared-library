b90a152ccb114789a30ffb0cd3ae2deb5959e581
NKV <35300907+nkvthedev@users.noreply.github.com>
2022-06-22 15:38:53 -0400

    Update Instance.groovy

--- src/com/local/Instance.groovy
+++ src/com/local/Instance.groovy
@@ -3,13 +3,17 @@ package com.local
 class Instance {
   def script = null
   def name = ''
+  def age = null
 
   Instance(def script, String input_name) {
     this.script = script
     this.name = input_name
+  }
+
+  def set_age() {
     age = 20
   }
-  
+
   def running_tests() {
     this.script.execute()
   }
