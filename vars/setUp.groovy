d133f3806f96d0daabac82c7602ceba5e1ce0d93
NKV <35300907+nkvthedev@users.noreply.github.com>
2022-06-22 14:38:48 -0400

    Update setUp.groovy

--- vars/setUp.groovy
+++ vars/setUp.groovy
@@ -3,11 +3,7 @@ import com.local.Instance
 def call(String name) {  
   def i = new Instance(this, name)
   println i.name
-  println i.default_addr
-  println i.default_name
   
   def j = new Instance(this, 'ANOTHER_NAME')
-  println i.name
-  println i.default_addr
-  println i.default_name
+  println j.name
 }
