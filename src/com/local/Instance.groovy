7ead2e618bf7ab2bbdb37a48b08347621391a89d
NKV <35300907+nkvthedev@users.noreply.github.com>
2022-06-22 14:30:23 -0400

    Update Instance.groovy

--- src/com/local/Instance.groovy
+++ src/com/local/Instance.groovy
@@ -1,20 +1,8 @@
 package com.local
 
 class Instance {
-  def default_name = 'DEFAULT_NAME'
-  def default_addr = 'DEFAULT_ADDRESS'
-  def script = null
-
   Instance(def script, String input_name) {
     this.script = script
-    this.name = 'NAME1'
-  }
-  
-  def getAddress() {
-    return this.default_addr
-  }
-  
-  def setAddress() {
-    this.default_addr = 'NEW_ADDRESS'
-  }
+    this.name = input_name
+  }  
 }
