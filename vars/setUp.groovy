819375035075bf4a2ebdc3ee28b60f47b463f7d9
NKV <35300907+nkvthedev@users.noreply.github.com>
2022-06-23 13:30:02 -0400

    Update setUp.groovy

--- vars/setUp.groovy
+++ vars/setUp.groovy
@@ -1,10 +1,14 @@
 import com.local.Instance
 
 def call(String name, def script) {
-  println this
-  println job_name
-//   println this.job_name
-//   println script.jn
+  println this // 'this' here refers to the object of the Jenkinsfile class.
+  // Any variable defined in the Jenkinsfile using the "def" keyword becomes like a property of a class named Jenkinsfile. There is one object of this
+  // Jenkinsfile class created when running a pipeline job and all singleton classes defined inside the vars folder act as methods of this class, and
+  // hence these singletons are able to access the properties using either the name of the property or this.<property>.
+  println job_name // 'job_name' is not defined in this groovy file, instead it is defined in the Jenkinsfile using "def jobname" and acts as a
+  // property of the class Jenkinsfile. This property is therefore also accessible using 'this.job_name' below.
+  println this.job_name
+  println script.jn
   this.echo "test1"
   echo "test2"
   execute()
