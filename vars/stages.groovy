0bd780453f5eaf76d78e5ecc5e1ad415e808cbf9
NKV <35300907+nkvthedev@users.noreply.github.com>
2022-06-23 17:19:02 -0400

    Create stages.groovy

---
+++ vars/stages.groovy
@@ -0,0 +1,14 @@
+def run() {
+  return {
+    stage('stage1') {
+      steps {
+        echo 'Running stage1'
+      }
+    }
+    stage('stage2') {
+      steps {
+        echo 'Running stage2'
+      }
+    }
+  }
+}
