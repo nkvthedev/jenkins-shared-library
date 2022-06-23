caabd2bf173e4ad9b2b1daae553dafd12fc50a85
NKV <35300907+nkvthedev@users.noreply.github.com>
2022-06-23 15:22:24 -0400

    Update setUp.groovy

--- vars/setUp.groovy
+++ vars/setUp.groovy
@@ -9,9 +9,16 @@ def call(String name, def script) {
   // by using the def keyword, because Jenkinfile acts as a parent class of the singletons defined inside the vars folder.
   // Likewise the other singletons defined inside the vars folder can directly access any other singleton of the vars folder by just calling it by 
   // its name or using this.<singleton> to invoke the method.
-  println job_name // 'job_name' is not defined in this groovy file, instead it is defined in the Jenkinsfile using "def jobname" and acts as a
-  // property of the class Jenkinsfile. This property is therefore also accessible using 'this.job_name' below.
+  println job_name // Since 'job_name' is not assigned a value as such in this singleton and instead is just being used so Groovy looks for the
+  // resence of the variable 'job_name' as a property in the jenkinsfile. And if it finds it to be defined as a variable in the jenkinsfile then
+  // it used that value in this statement. A property can either be accessed directly like 'job_name' or by using the this keyword like 
+  // 'this.job_name' below.
   println this.job_name
+
+  // If 'job_name' was not defined as a property in the parent of this singleton i.e. in the Jenkinsfile class then it would be deinfed as a property
+  // of this child singleton class when using it as a variable in the left hand side of a statement like below.
+  job_name1 = 'no_name'
+
   println script.jn
   this.echo "test1"
   echo "test2"
@@ -31,8 +38,9 @@ def call(String name, def script) {
   println i.name
   println j.name
   
-  param1 = 'param1' // Since param1 is not defined in this method using the def keyword so jenkins refers to the param1 as a "property" of the object
-  // created for the Jenkinsfile class. This property must therefore be defined within the Jenkinsfile using the def keyword.
-  this.param2 = 'param2'
+  param1 = 'param1' // Since param1 is not defined in this method using the def keyword so jenkins defines param1 as a "property" of the singleton 
+  // object.
+  this.param2 = 'param2' // param2 would be defined as a property of this singleton in case it is not defined elsewhere before it's use in this
+  // statement.
 
 }
